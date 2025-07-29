428bbc4d2f8ed742c7163fce793c2e0d1a0ecf5c
liyong1 <liyong1@yy.com>
2025-07-29 14:31:47 +0800

    更新接口

--- annotation/src/main/java/com/yy/core/room/protocol/BaseEntity.java
+++ annotation/src/main/java/com/yy/core/room/protocol/BaseEntity.java
@@ -1,22 +1,25 @@
 package com.yy.core.room.protocol;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
  * Created by liyong on 2017/5/29.
  */
 
 public class BaseEntity {
-    public int result;
-    public String msg;
+    public int result = 0;
+    @Nullable
+    public String msg = "";
 
-    public boolean isSuccess(){
+    public boolean isSuccess() {
         return result == 0;
     }
 
     @Override
     public String toString() {
         return "BaseEntity{" +
-                "result=" + result +
-                ", msg='" + msg + '\'' +
-                '}';
+            "result=" + result +
+            ", msg='" + msg + '\'' +
+            '}';
     }
 }
