e74d4c7e78c87524debc11b3ac3b873a1a1923cf
liyong1 <liyong1@yy.com>
2025-07-29 17:19:59 +0800

    将方法改为open

--- annotation/src/main/java/com/yy/core/room/protocol/BaseEntity.kt
+++ annotation/src/main/java/com/yy/core/room/protocol/BaseEntity.kt
@@ -4,7 +4,10 @@ package com.yy.core.room.protocol
  * Created by liyong on 2017/5/29.
  */
 open class BaseEntity {
+    @JvmField
     var result: Int = 0
+
+    @JvmField
     var msg: String? = ""
 
     open fun isSuccess(): Boolean {
---
+++ app/src/main/java/com/allen/android/smartflyper/UserEntity2.java
@@ -0,0 +1,23 @@
+package com.allen.android.smartflyper;
+
+import com.yy.core.room.protocol.BaseEntity;
+
+/**
+ * Time:2025/7/29 17:18
+ * Author:
+ * Description:
+ */
+public class UserEntity2 extends BaseEntity {
+
+    int age=10;
+
+    @Override
+    public String toString() {
+        return "UserEntity2{" +
+            "age=" + age +
+            "age=" + result +
+            "age=" + msg +
+            "age=" + isSuccess() +
+            '}';
+    }
+}
